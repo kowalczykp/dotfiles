02cc55e6cd33b80e8ced747cda4aa4800e6a8829
Jon Doveston <jon.doveston@onthebeach.co.uk>
2013-09-27 10:18:41 +0100

    tweak the zephyros coords

--- zephyros.rb
+++ zephyros.rb
@@ -1,25 +1,31 @@
 # encoding: UTF-8
-# vim: set ft=ruby fenc=UTF-8 sw=2 ts=2 et:
 require '/Applications/Zephyros.app/Contents/Resources/libs/zephyros.rb'
 
 mash = ['cmd', 'alt', 'ctrl']
 
+def full_frame(screen)
+  # this is to tweak the full screen
+  frame = screen.frame_without_dock_or_menu
+  frame.x -= 4
+  frame.w += 4
+  frame
+end
+
 API.bind 'SPACE', mash do
   win = API.focused_window
-  frame = win.screen.frame_without_dock_or_menu
-  win.frame = frame
+  win.frame = full_frame(win.screen)
 end
 
 API.bind 'UP', mash do
   win = API.focused_window
-  frame = win.screen.frame_without_dock_or_menu
+  frame = full_frame(win.screen)
   frame.h /= 2
   win.frame = frame
 end
 
 API.bind 'DOWN', mash do
   win = API.focused_window
-  frame = win.screen.frame_without_dock_or_menu
+  frame = full_frame(win.screen)
   frame.y += frame.h / 2
   frame.h /= 2
   win.frame = frame
@@ -27,14 +33,14 @@ end
 
 API.bind 'LEFT', mash do
   win = API.focused_window
-  frame = win.screen.frame_without_dock_or_menu
+  frame = full_frame(win.screen)
   frame.w /= 2
   win.frame = frame
 end
 
 API.bind 'RIGHT', mash do
   win = API.focused_window
-  frame = win.screen.frame_without_dock_or_menu
+  frame = full_frame(win.screen)
   frame.x += frame.w / 2
   frame.w /= 2
   win.frame = frame

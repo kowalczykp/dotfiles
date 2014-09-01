ab915350de35a8c6ff78fbed38a92cb2841f7c08
Jon Doveston <jon.doveston@onthebeach.co.uk>
2014-09-01 14:10:46 +0100

    swap brewfile for a simple bash script

---
+++ brewfile.sh
@@ -0,0 +1,98 @@
+brew update
+#brew upgrade
+
+brew tap homebrew/binary
+brew tap homebrew/versions
+
+brew install mercurial
+brew install tig
+brew install git
+brew install git-extras
+brew install git-sh
+
+#install mongodb
+#install mysql
+#install redis
+#install sqlite
+#install haproxy
+#install nginx
+#install nodejs
+#install postgresql
+
+brew install libtool
+brew install libyaml
+
+brew install autoconf
+brew install automake
+brew install pkg-config
+brew install readline
+brew install valgrind
+brew install gcc
+#install gcc48
+#install gcc49
+
+#install haskell-platform --cc=/usr/local/bin/gcc-4.8
+brew install chruby
+brew install ruby-install
+#install chicken
+
+#install casperjs
+#install phantomjs
+
+brew install bash
+brew install bash-completion
+brew install coreutils
+brew install ack
+brew install wget
+brew install ctags
+brew install htop
+brew install watch
+brew install vim
+brew install tmux
+brew install ranger
+brew install clamav
+brew install cowsay
+brew install wakeonlan
+brew install tree
+brew install ffmpeg
+brew install ffmpeg2theora
+#install ssh-copy-id
+#install s3cmd
+#install fdupes
+#install openssl
+#install markdown
+#install duplicity
+#install gd
+#install graphviz --with-pdf
+#install reattach-to-user-namespace
+#install docker
+#install id3tool
+#install imagemagick
+#install unrar
+#install libdvdcss
+#install dos2unix
+#install nmap
+#install archey
+
+brew tap caskroom/cask
+brew install brew-cask
+
+# Virtualization
+#cask install vagrant
+#cask install virtualbox
+
+# Applications
+#cask install adium
+#cask install git-annex
+#cask install google-chrome
+#cask install google-drive
+#cask install iterm2
+#cask install skype
+#cask install sequel-pro
+#cask install vlc
+#cask install plex
+#cask install skype
+#cask install unison-usenet
+#cask install disk-inventory-x
+
+brew cleanup

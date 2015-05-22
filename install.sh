a5b6cbdd1d2d85039b1af454f4ef6f63c5ced8d3
Jon Doveston <jon.doveston@onthebeach.co.uk>
2015-05-22 11:39:24 +0100

    add gemrc for docs

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for file in vimrc tmux.conf bash_profile bashrc profile ackrc vim gitignore inputrc psqlrc jshintrc
+for file in vimrc tmux.conf bash_profile bashrc profile ackrc vim gitignore inputrc psqlrc jshintrc gemrc
 do
   if [ -a $HOME/.${file} ]
   then echo ".$file found, doing nothing"

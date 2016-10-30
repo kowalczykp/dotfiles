b7777c5348df6846e704265d362005c2878780f7
Jon Doveston <jon@doveston.me.uk>
2016-10-30 22:05:50 +0000

    More brewfile refactors and some spring cleaning

--- brewfile.sh
+++ brewfile.sh
@@ -1,3 +1,10 @@
+function install {
+  if brew cask list | grep -q $1
+  then echo "Already installed $1"
+  else brew cask install $1
+  fi
+}
+
 brew tap homebrew/binary
 brew tap homebrew/versions
 brew tap neovim/homebrew-neovim
@@ -5,7 +12,6 @@ brew tap manastech/crystal
 brew tap universal-ctags/universal-ctags
 brew tap homebrew/science
 brew tap buo/cask-upgrade
-
 brew tap caskroom/cask
 brew tap caskroom/fonts
 brew tap caskroom/versions
@@ -102,32 +108,49 @@ brew install python
 brew install --HEAD xhyve
 brew install dnsmasq
 
-brew install caskroom/cask/java
+brew install --HEAD neovim
+brew install crystal-lang --with-llvm
+brew install --HEAD universal-ctags
+
+# Science
+brew install gnuplot --with-qt --with-x11
+brew install ghostscript
+brew install octave --with-x11
+brew install fltk
 
 # Applications
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install skype
-brew cask install sequel-pro
-brew cask install vlc
-brew cask install flash
-brew cask install firefox
-brew cask install viscosity
-brew cask install sqlitebrowser
-brew cask install evernote
-brew cask install gpgtools
-brew cask install slack
-brew cask install mou
-brew cask install wkhtmltopdf
-brew cask install aerial
-brew cask install flux
-brew cask install recordit
-brew cask install clipgrab
-brew cask install omnidisksweeper
-brew cask install torbrowser
-brew cask install tagspaces
+install vagrant
+install virtualbox
+install google-chrome
+install iterm2
+install skype
+install sequel-pro
+install vlc
+install flash-player
+install firefox
+install viscosity
+install sqlitebrowser
+install evernote
+install gpgtools
+install slack
+install mou
+install wkhtmltopdf
+install aerial
+install flux
+install recordit
+install clipgrab
+install omnidisksweeper
+install torbrowser
+install tagspaces
+install pgadmin3
+install avidemux
+install audacity
+install aegisub
+install duet
+install mactex
+install font-fontawesome
+install xquartz
+install java
 
 # brew cask install paparazzi
 # brew cask install appcleaner
@@ -139,33 +162,10 @@ brew cask install tagspaces
 # brew cask install calibre
 # brew cask install musicbrainz-picard
 # brew cask install dropbox
-
 #cask install plex
 #cask install unison-usenet
 #cask install adium
 #cask install git-annex
 
-brew install caskroom/cask/avidemux
-brew install caskroom/cask/audacity
-brew install caskroom/cask/aegisub
-
-brew cask install caskroom/cask/pgadmin3
-brew cask install duet
-
-brew install --HEAD neovim
-brew install crystal-lang --with-llvm
-brew install --HEAD universal-ctags
-
-# Science
-brew install gnuplot --with-qt --with-x11
-brew cask install mactex
-brew cask install font-fontawesome
-brew cask install xquartz
-brew install ghostscript
-brew install octave --with-x11
-brew install fltk
-
 brew cleanup
 brew cask cleanup
-
-pip install glances
